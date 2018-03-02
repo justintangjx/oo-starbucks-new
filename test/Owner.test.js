25215239eb344d93439e3dd0c821065ce5c6b9bb
“justintangjx” <just.think.tang@gmail.com>
2018-03-02 22:42:13 +0800

    final

--- test/Owner.test.js
+++ test/Owner.test.js
@@ -1,6 +1,6 @@
 var Owner = require("../src/Owner");
 var Item = require("../src/Item");
-var Order = require("../src/Order")
+var Order = require("../src/Order");
 describe("Owner", () => {
   it("should create an owner successfully", () => {
     var owner = new Owner();
@@ -10,12 +10,11 @@ describe("Owner", () => {
   });
 
   it("should create an order with item and customer's name", () => {
-    
     var item = new Item("tea", 5);
     var customerName = "justin";
-    
+
     var expectedOrder = new Order(item, customerName);
-    var testVar = [{firstName: 'chee'}];
+    var testVar = [{ firstName: "chee" }];
     var owner = new Owner();
 
     expect(owner.createOrder(expectedOrder)).toEqual(owner.ordersList);
